37c2676701d5d040d843853732fd30720188ace7
Andres Leonardo <andresleonardobg@gmail.com>
2025-02-19 22:04:01 -0500

    feat(config): :wrench: configuration files updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/andresleonardo/',
+  base: "/andresleonardo/",
   plugins: [vue()],
   build: {
-    target: 'esnext'
-  }
-})
+    target: "esnext",
+  },
+  define: {
+    // Añade estas definiciones
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "false",
+    __VUE_OPTIONS_API__: "true",
+    __VUE_PROD_DEVTOOLS__: "false",
+  },
+});
