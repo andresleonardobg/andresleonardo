a0cc41e15f54459aa3aff2b9643e16e2be4ecc90
Andres Leonardo <andresleonardobg@gmail.com>
2025-04-21 23:40:00 -0500

    refactor(vite config):

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/andresleonardo/",
+  base: "/portfolio/",
   plugins: [vue()],
   build: {
     target: "esnext",
